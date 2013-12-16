2a4200ba65f85128a4bdeecc9d8cc8a5bf40b5cf
Jeremy Meyer <jjman505@gmail.com>
2013-12-16 12:36:23 -0500

    Finished static pages

---
+++ spec/requests/static_pages_spec.rb
@@ -0,0 +1,40 @@
+require 'spec_helper'
+
+describe "StaticPages" do
+  describe "Home page" do
+    it "should have the right title" do
+      visit '/static_pages/home'
+      expect(page).to have_title("Home")
+    end
+    it "should have the content 'Sample App'" do
+      visit '/static_pages/home'
+      expect(page).to have_content('Sample App')
+    end
+  end
+end
+
+describe "StaticPages" do
+  describe "Help page" do
+    it "should have the right title" do
+      visit '/static_pages/help'
+      expect(page).to have_title("Help")
+    end
+    it "should have the content 'Help'" do
+      visit '/static_pages/help'
+      expect(page).to have_content('Help')
+    end
+  end
+end
+
+describe "StaticPages" do
+  describe "About page" do
+    it "should have the right title" do
+      visit '/static_pages/about'
+      expect(page).to have_title("About")
+    end
+    it "should have the content 'About'" do
+      visit '/static_pages/about'
+      expect(page).to have_content('About')
+    end
+  end
+end
